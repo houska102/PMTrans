d693bfbf255f489f8e11ed44580b2833fe72bbfe
hrujan <houska102@gmail.com>
2025-06-22 14:55:34 +0200

    css cleanup

--- script.js
+++ script.js
@@ -1,33 +1,55 @@
 const activeSection = window.location.hash
 
 window.onload = () => {
-  if (activeSection) {
-    document.querySelector('.main-nav a[href="' + activeSection + '"]').classList.add('active');
+  const navLinks = document.querySelectorAll('.nav-links a');
+
+  const selectMenuItem = (sectionId) => {
+    navLinks.forEach(link => {
+      if (link.getAttribute('href') === sectionId) {
+        link.classList.add('active');
+      } else {
+        link.classList.remove('active');
+      }
+    })
   }
-}
 
-const toggleMenu = () => {
-  document.querySelector('.nav-links').classList.toggle('open');
-  document.querySelector('.hamburger').classList.toggle('active');
-  document.getElementById('menu-backdrop').classList.toggle('active');
-}
+  if (activeSection) {
+    selectMenuItem(activeSection);
+  }
+  
 
-document.querySelectorAll('.nav-links a').forEach(link => {
-  link.addEventListener('click', function() {
-    const isBackdropActive = document.getElementById('menu-backdrop').classList.contains('active');
-    document.querySelector('.nav-links a.active')?.classList.remove('active');
-    this.classList.add('active');
-    if (isBackdropActive) {
-      toggleMenu();
+  const toggleMenu = () => {
+    document.querySelector('.nav-links').classList.toggle('open');
+    document.querySelector('.hamburger').classList.toggle('active');
+    document.getElementById('menu-backdrop').classList.toggle('active');
+  }
+
+  document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function() {
+      const isBackdropActive = document.getElementById('menu-backdrop').classList.contains('active');
+      if (isBackdropActive) {
+        toggleMenu();
+      }
+    });
+  });
+
+  document.getElementById('menu-backdrop').addEventListener('click', toggleMenu)
+
+  document.querySelector('.hamburger').addEventListener('click', toggleMenu)
+
+
+  const sections = document.querySelectorAll('#main-content>section');
+  window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+    for (let section of sections) {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const sectionId = section.getAttribute('id');
+
+      if (scrollPosition >= sectionTop - 100 && scrollPosition < sectionTop + sectionHeight) {
+        selectMenuItem(`#${sectionId}`);
+      }
     }
   });
-});
 
-document.querySelector('.main-nav .logo').addEventListener('click', () => {
-  document.querySelector('.nav-links a.active')?.classList?.remove('active');
-  document.querySelector('.nav-links a[href="#logistika"]')?.classList?.add('active');
-})
-
-document.getElementById('menu-backdrop').addEventListener('click', toggleMenu)
-
-document.querySelector('.hamburger').addEventListener('click', toggleMenu)
+}
